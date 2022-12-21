827aa0ad2db4c6a25c8fb466293d05f5150adb1c
titoworld.dev <titocristian.dev@gmail.com>
2022-12-21 17:55:35 +0100

    Loading indicator

--- src/components/ListOfGifs.js
+++ src/components/ListOfGifs.js
@@ -8,20 +8,28 @@ export default function ListOfGifs({ params }) {
   // El useState es un hook que nos permite manejar el estado de un componente
   // en React. El primer valor que retorna es el estado actual y el segundo
   // valor es una función que nos permite actualizar el estado.
-  const [gifs, setGifs] = useState([]);
+  const [gifs, setGifs] = useState({
+    loading: false,
+    results: []
+  });
 
   // El useEffect se ejecuta cuando el componente se renderiza por primera vez
   // y cuando el componente se actualiza
   useEffect(() => {
-    getGifs({ keyword }).then((gifs) => setGifs(gifs));
+    setGifs({ loading: true });
+    getGifs({ keyword }).then((gifs) => {
+      setGifs({ loading: false, results: gifs });
+    });
   }, [keyword]); // El segundo argumento es un array de dependencias, si el array
   // esta vacio, el useEffect se ejecutará solo una vez, si el array tiene
   // dependencias, el useEffect se ejecutará cada vez que una de las dependencias
   // cambie de valor
 
+  if (gifs.loading) return <i>Cargando...🌀</i>;
+
   return (
     <>
-      {gifs.map(({ id, title, url }) => (
+      {gifs.results.map(({ id, title, url }) => (
         <Gif id={id} key={id} title={title} url={url} />
       ))}
     </>
