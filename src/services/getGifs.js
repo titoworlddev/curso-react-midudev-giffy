15b0f3a8a6200a8bb1753c8907cbba9e4a8a932b
titoworlddev <titocristian.dev@gmail.com>
2023-01-04 18:45:03 +0100

    Actualizacion

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,11 +1,12 @@
 import { API_KEY, API_URL } from './settings';
 
-const fromApiResponseToGifs = (apiResponse) => {
+const fromApiResponseToGifs = apiResponse => {
   const { data = [] } = apiResponse;
   if (Array.isArray(data)) {
-    const gifs = data.map((image) => {
+    const gifs = data.map(image => {
       const { images, title, id } = image;
       const { url } = images.downsized_medium;
+
       return { title, id, url };
     });
     return gifs;
@@ -17,6 +18,6 @@ export default function getGifs({ limit = 25, keyword = 'morty' } = {}) {
   const apiURL = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&offset=0&rating=G&lang=en`;
 
   return fetch(apiURL)
-    .then((res) => res.json())
+    .then(res => res.json())
     .then(fromApiResponseToGifs);
 }
