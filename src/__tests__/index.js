208b74d15261ee8f6f00d2090985a9f2757181b2
titoworlddev <titocristian.dev@gmail.com>
2023-01-23 22:33:39 +0100

    Testing

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -4,7 +4,7 @@ import App from '../App';
 
 // primer argumento es el nombre del test
 // segundo argumento es una función que contiene el test
-test('home work as expected', () => {
+test('home works as expected', () => {
   render(<App />);
   const linkElement = screen.getByText(/Última búsqueda/i);
   expect(linkElement).toBeInTheDocument();
