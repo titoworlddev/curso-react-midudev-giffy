53b75557f6deb1ffb59acf44423b7decd4deebb5
titoworlddev <titocristian.dev@gmail.com>
2023-01-23 16:07:50 +0100

    Making the tests

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 // primer argumento es el nombre del test
 // segundo argumento es una función que contiene el test
-test('renders without crashing', () => {
+test('home work as expected', () => {
   render(<App />);
   const linkElement = screen.getByText(/Última búsqueda/i);
   expect(linkElement).toBeInTheDocument();
